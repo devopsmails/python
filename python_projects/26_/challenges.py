4a01f83d92d100bdec9026ca6202d412cc6e4dda
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-04-10 15:42:04 +0530

    Update challenges.py

--- python_projects/26_/challenges.py
+++ python_projects/26_/challenges.py
@@ -47,3 +47,29 @@ Results:
 =======
 {'What': 4, 'is': 2, 'the': 3, 'Airspeed': 8, 'Velocity': 8, 'of': 2, 'an': 2, 'Unladen': 7, 'Swallow?': 8}
 
+4.
+Instructions
+You are going to use Dictionary Comprehension to create a dictionary called weather_f that takes
+each temperature in degrees Celsius and converts it into degrees Fahrenheit.
+
+To convert temp_c into temp_f:
+
+(temp_c * 9/5) + 32 = temp_f
+
+weather_c = {
+    "Monday": 12,
+    "Tuesday": 14,
+    "Wednesday": 15,
+    "Thursday": 14,
+    "Friday": 21,
+    "Saturday": 22,
+    "Sunday": 24,
+}
+weather_f = {days : temp * 9/5 + 32 for (days, temp)  in weather_c.items() }
+
+print(weather_f)
+
+Results:
+ =====
+
+{'Monday': 53.6, 'Tuesday': 57.2, 'Wednesday': 59.0, 'Thursday': 57.2, 'Friday': 69.8, 'Saturday': 71.6, 'Sunday': 75.2}
