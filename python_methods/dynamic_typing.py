8083a35eef2030023c7eb269ec48921e76f6fc96
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-04-18 16:12:54 +0530

    Create dynamic_typing.py

---
+++ python_methods/dynamic_typing.py
@@ -0,0 +1,9 @@
+Dynamic Typing:
+  
+  By using this we can type and an interger as a string or viseversa which is allowed through the variables.
+  
+  ex:
+    
+  minutes = 5
+  if minutes == 0: #0 = string here
+    minutes = "00" #"00" which is a string here#
