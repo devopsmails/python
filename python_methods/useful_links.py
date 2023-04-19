647e7700a68764e4976a765628b6c1c4c1b325ea
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-04-19 12:42:04 +0530

    Update useful_links.py

--- python_methods/useful_links.py
+++ python_methods/useful_links.py
@@ -1,2 +1,5 @@
+Python best Docs - https://www.w3schools.com/python/python_file_write.asp
 Color - https://colorhunt.co/
 TKinter - https://www.tcl.tk/man/tcl8.6/contents.html
+Canvas - https://tkdocs.com/tutorial/canvas.html
+Basic_Widget - https://tkdocs.com/tutorial/widgets.html#entry
