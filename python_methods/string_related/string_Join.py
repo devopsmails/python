f4eafca5df231ed24102315e48d2e5ab95bd6290
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-04-19 15:14:45 +0530

    Create string_Join.py

---
+++ python_methods/string_related/string_Join.py
@@ -0,0 +1,14 @@
+string Join() Method:
+  
+  Join all items in a tuple into a string, using a hash character as separator:
+
+myTuple = ("John", "Peter", "Vicky")
+
+x = "#".join(myTuple)
+
+print(x)
+
+results:
+====
+ 
+John#Peter#Vicky
