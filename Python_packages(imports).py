01ac221c3e50358aca9bbae67c9bfe2adf31fa74
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-04-20 16:04:31 +0530

    Update and rename Python_packages.py to Python_packages(imports).py

---
+++ Python_packages(imports).py
@@ -0,0 +1,9 @@
+import json #which is a default module in Python package
+import random
+import math 
+import tkinter #framing
+import turtle #framing
+import pyperclip
+
+pyperclip :(Helps to copy the info to clipboard).
+https://pypi.org/project/pyperclip/
