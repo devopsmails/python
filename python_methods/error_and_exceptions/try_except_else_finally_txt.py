a7d8ee45eff8d57c96937e3e377e6d761c16f4eb
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-04-20 15:33:38 +0530

    Update try_except_else_finally_txt.py

--- python_methods/error_and_exceptions/try_except_else_finally_txt.py
+++ python_methods/error_and_exceptions/try_except_else_finally_txt.py
@@ -43,3 +43,24 @@ something
 File was closed
 
 --------------
+type: 2
+=======
+
+facebook_posts = [
+    {'Likes': 21, 'Comments': 2}, 
+    {'Likes': 13, 'Comments': 2, 'Shares': 1}, 
+    {'Likes': 33, 'Comments': 8, 'Shares': 3}, 
+    {'Comments': 4, 'Shares': 2}, 
+    {'Comments': 1, 'Shares': 1}, 
+    {'Likes': 19, 'Comments': 3}
+]
+
+total_likes = 0
+
+for post in facebook_posts:
+    try:
+        total_likes = total_likes + post['Likes']
+    except KeyError:
+        pass #pass it to the next line of code if it finds the same error
+
+print(total_likes)
