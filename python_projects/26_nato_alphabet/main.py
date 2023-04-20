80b907b1ebdbdc68f86e43c2c79374e345a393ba
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-04-20 15:49:21 +0530

    Update main.py

--- python_projects/26_nato_alphabet/main.py
+++ python_projects/26_nato_alphabet/main.py
@@ -1,4 +1,3 @@
-
 import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
@@ -6,7 +5,12 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter : row.code for (index, row) in data.iterrows()}
 # print(phonetic_dict)
 word = input("Enter a word: " ).upper()
-output = [phonetic_dict[char] for char in word]
-print(output)
-
- 
+#Helps excepting a keyerror & to re take the input
+def phonetic_alphabet():
+    try:
+        output = [phonetic_dict[char] for char in word]
+    except KeyError:
+        print("Only Alphabet letters please!")
+        phonetic_alphabet()
+    print(output)
+phonetic_alphabet()
