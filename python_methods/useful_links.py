30abc59a0bf5de95745743e172c8cfeb7ada66d6
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-04-28 14:37:19 +0530

    Update useful_links.py

--- python_methods/useful_links.py
+++ python_methods/useful_links.py
@@ -3,3 +3,4 @@ Color - https://colorhunt.co/
 TKinter - https://www.tcl.tk/man/tcl8.6/contents.html
 Canvas - https://tkdocs.com/tutorial/canvas.html
 Basic_Widget - https://tkdocs.com/tutorial/widgets.html#entry
+Hosting python on cloud for free - https://www.pythonanywhere.com/
