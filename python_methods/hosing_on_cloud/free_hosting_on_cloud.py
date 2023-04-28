fe6e87afea9576ab487e24257a50711007d3cc0f
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-04-28 16:00:09 +0530

    Create free_hosting_on_cloud.py

---
+++ python_methods/hosing_on_cloud/free_hosting_on_cloud.py
@@ -0,0 +1,9 @@
+https://www.pythonanywhere.com/user/PythonwithDevops/tasks_tab/
+  
+Need to signup to use hosting services.
+
+1. DashBoard>Files> Upload files or folders which are required to run the program
+2. To run manually = Dashboard>Consoles>$python3 main.py > pops up completed = successful execution
+3. To Run Autometically = Dashboard>Tasks> Enter the UTC time to run the program >
+    COMMAND: python3 main.py> create > click on task log: to know the status.
+4. Only one free task is allowed at a time.
