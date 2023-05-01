aabeee6f9c93587cd7cc4b1ee86bfc1b564d4b85
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-05-01 11:27:17 +0530

    Update useful_links.py

--- python_methods/useful_links.py
+++ python_methods/useful_links.py
@@ -1,6 +1,7 @@
-Python best Docs - https://www.w3schools.com/python/python_file_write.asp
-Color - https://colorhunt.co/
-TKinter - https://www.tcl.tk/man/tcl8.6/contents.html
-Canvas - https://tkdocs.com/tutorial/canvas.html
-Basic_Widget - https://tkdocs.com/tutorial/widgets.html#entry
-Hosting python on cloud for free - https://www.pythonanywhere.com/
+Python best Docs :- https://www.w3schools.com/python/python_file_write.asp
+Color :- https://colorhunt.co/
+TKinter :- https://www.tcl.tk/man/tcl8.6/contents.html
+Canvas :- https://tkdocs.com/tutorial/canvas.html
+Basic_Widget :- https://tkdocs.com/tutorial/widgets.html#entry
+Hosting python on cloud for free:- https://www.pythonanywhere.com/
+HTTPresponse code:- https://www.webfx.com/web-development/glossary/http-status-codes/
