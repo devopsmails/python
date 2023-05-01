fa505e06d7f245780663666fc7cc32b0c2579775
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-05-01 11:18:32 +0530

    Create api_response_codes.py

---
+++ python_methods/api/api_response_codes.py
@@ -0,0 +1,9 @@
+404 = API doesn't exist
+
+shortcut code:
+  
+  1xx = Hold On(needs some more time)
+  2xx = Here You go(done as expected)
+  3xx = Go Away (You have no Permission)
+  4xx = You Screwed Up(Fault from Your side)
+  5xx = I Screwed Up(Fault from Server Side)
