1c150a6599cb62f5ec2d0e70075546cea4559ea4
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-05-01 11:49:38 +0530

    Update api.py

--- python_methods/api/api.py
+++ python_methods/api/api.py
@@ -5,3 +5,17 @@ API = Application Programming Interface
   
   Your Program ------------>> API(Request with proper rules) ----------->> External System
   Your program <<------------ API(Response(data))<<----------------------  External System
+  
+Example:
+  
+import requests # helps to call the api requests
+
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+data = response.json()
+longitude = data["iss_position"]["longitude"]
+latitude = data["iss_position"]["latitude"]
+iss_position = (longitude, latitude)
+print(iss_position)
+
+Results:
+('-100.1727', '12.3296')
