04bdd7b7e75f83b506afed6d3ad479ad7314e9c9
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-05-01 11:12:44 +0530

    Update and rename .py to api.py

---
+++ python_methods/api/api.py
@@ -0,0 +1,7 @@
+API = Application Programming Interface
+
+  => Is a set of commands, functions, protocols, and Objects that 
+  programmers can use to create software or interact with external system,
+  
+  Your Program ------------>> API(Request with proper rules) ----------->> External System
+  Your program <<------------ API(Response(data))<<----------------------  External System
