2f757114876c27cdbf64b27620b90d87279de76a
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-05-04 16:24:02 +0530

    Create typing_hints.py

---
+++ python_methods/debugging/typing_hints.py
@@ -0,0 +1,7 @@
+Typing Hints:
+  
+  Helps to declare the data type at the earlist before we create the bugs else they show a pop for declared data type.
+  
+  ex:
+    def greeting(name: str) -> str:
+        return "hello" + name
