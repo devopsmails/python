3d00c46063d592d8f336c2c16fb7b9b6dbf9441b
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-05-06 13:10:09 +0530

    Update creds.py

--- python_methods/creds.py
+++ python_methods/creds.py
@@ -3,3 +3,11 @@ https://www.pythonanywhere.com/
 Username: PythonwithDevops0
 email   : devopsmails1@gmail.com
 pwd     : Devops@1  
+===============
+
+https://home.openweathermap.org/home
+  Helps to know the whether forecast
+Username: Python
+Email: devopsmails1@gmail.com
+PWD: Devops@1
+
