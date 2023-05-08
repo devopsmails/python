79a10aca5f4239b944efd07ed6552da6db1f8e38
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-05-08 10:39:14 +0530

    Update useful_links.py

--- python_methods/useful_links.py
+++ python_methods/useful_links.py
@@ -1,11 +1,22 @@
-Python best Docs :- https://www.w3schools.com/python/python_file_write.asp
-Color :- https://colorhunt.co/
-TKinter :- https://www.tcl.tk/man/tcl8.6/contents.html
-Canvas :- https://tkdocs.com/tutorial/canvas.html
-Basic_Widget :- https://tkdocs.com/tutorial/widgets.html#entry
-Hosting python on cloud for free:- https://www.pythonanywhere.com/
-HTTPresponse code:- https://www.webfx.com/web-development/glossary/http-status-codes/
-Map Point with lat & longtude:- https://www.latlong.net/Show-Latitude-Longitude.html
-Travia db:- https://opentdb.com/api_config.php #Helps to get some question type db
-open/paid weather forcasting website:- https://openweathermap.org/api
-    
+Python best Docs: 
+    https://www.w3schools.com/python/python_file_write.asp
+Color : 
+    https://colorhunt.co/
+TKinter : 
+    https://www.tcl.tk/man/tcl8.6/contents.html
+Canvas : 
+    https://tkdocs.com/tutorial/canvas.html
+Basic_Widget : 
+    https://tkdocs.com/tutorial/widgets.html#entry
+Hosting python on cloud for free: 
+    https://www.pythonanywhere.com/
+HTTPresponse code:
+    https://www.webfx.com/web-development/glossary/http-status-codes/
+Map Point with lat & longtude: 
+    https://www.latlong.net/Show-Latitude-Longitude.html
+Travia db: 
+    https://opentdb.com/api_config.php #Helps to get some question type db
+open/paid weather forcasting website:
+    https://openweathermap.org/api
+Json Viewer:
+    https://jsonviewer.stack.hu/
