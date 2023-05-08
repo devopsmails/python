2672d46dc6e75254a80dd539e1752a9aeb47f2a6
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-05-08 10:19:21 +0530

    Update main.py

--- python_projects/33_iss_overhead_locater/main.py
+++ python_projects/33_iss_overhead_locater/main.py
@@ -3,8 +3,8 @@ from datetime import datetime
 import smtplib
 import time
 
-MY_EMAIL = "___YOUR_EMAIL_HERE____"
-MY_PASSWORD = "___YOUR_PASSWORD_HERE___"
+MY_EMAIL = "devopsmails1@gmail.com"
+MY_PASSWORD = "darvcqkwahuswzvo"
 MY_LAT = 51.507351 # Your latitude
 MY_LONG = -0.127758 # Your longitude
 
@@ -43,7 +43,7 @@ def is_night():
 while True:
     time.sleep(60)
     if is_iss_overhead() and is_night():
-        connection = smtplib.SMTP("__YOUR_SMTP_ADDRESS_HERE___")
+        connection = smtplib.SMTP("smtp.gmail.com")
         connection.starttls()
         connection.login(MY_EMAIL, MY_PASSWORD)
         connection.sendmail(
@@ -51,5 +51,3 @@ while True:
             to_addrs=MY_EMAIL,
             msg="Subject:Look Up👆\n\nThe ISS is above you in the sky."
         )
-
-
