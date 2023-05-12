4118b607ea3d902d9dd57347ea2ec38a24a5deb0
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-05-12 16:32:02 +0530

    Create python_datetime.py

---
+++ python_methods/string_related/python_datetime.py
@@ -0,0 +1,8 @@
+Python Date_time:
+  https://www.w3schools.com/python/python_datetime.asp
+    
+  We can format the date as per our requirement using python datetime method.
+  
+  from datetime import datetime
+  today = datetime.now()
+  formatted_date = today.strftime("%Y%m%d")
