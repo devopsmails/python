4bfeae20a3e799ea66cf912377ab861133537142
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-05-17 12:25:11 +0530

    Create HTML.py

---
+++ python_methods/HTML/HTML.py
@@ -0,0 +1,14 @@
+HTML:
+  
+  Hypertext = Hyperlink(which helps to route to different pages)
+  Markup = We can give special significance to that sentence ex: quotes("") bold, underline etc.
+  Language
+  
+  
+  EX:
+    
+  <h1>Hello World <h1/>
+  
+  <h1> = opening tag
+  <h1/> = closing tag
+  <h1>Hello World <h1/> = Elements
