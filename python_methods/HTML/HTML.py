337b9f5812ad1c40704bb82014e2182eaea30989
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-05-17 12:28:57 +0530

    Update HTML.py

--- python_methods/HTML/HTML.py
+++ python_methods/HTML/HTML.py
@@ -12,3 +12,14 @@ HTML:
   <h1> = opening tag
   <h1/> = closing tag
   <h1>Hello World <h1/> = Elements
+
+ Heading:
+  
+ Note: Only 6 heading are there not more than that.
+  
+  <h1>heading 1 </h1>
+  <h2>heading 2 </h2>
+  <h3>heading 3 </h3>
+  <h4>heading 4 </h4>
+  <h5>heading 5 </h5>
+  <h6>heading 6 </h6>
