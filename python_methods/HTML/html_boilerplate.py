6c82c1b03c03ff7ba8c0b0efdf5dbc2e88c6ec9c
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-05-18 11:51:05 +0530

    Create html_boilerplate.py

---
+++ python_methods/HTML/html_boilerplate.py
@@ -0,0 +1,13 @@
+The structure to follow for writting HTML code 
+<!DOCTYPE html>
+<html lang="en">
+    <head>#this code will not be seen by user
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Suresh</title>
+    </head>
+    <body>#Only this a user can view!
+        <p>It's my first website</p>
+    </body>
+</html>
