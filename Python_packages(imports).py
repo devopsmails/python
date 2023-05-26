580a76e45e9312fa150ac542e1088beb953a89ed
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-05-26 14:57:01 +0530

    Update Python_packages(imports).py

--- Python_packages(imports).py
+++ Python_packages(imports).py
@@ -7,6 +7,8 @@ import pyperclip
 import smtplib #helps sending mails
 import datetime #Helps to cath hold the date& time to execute specific action
 import requests # helps to call the api requests
+from bs4 import BeautifulSoup #for web scraping
+impor selenium #web testing
 
 pyperclip :(Helps to copy the info to clipboard).
 https://pypi.org/project/pyperclip/
