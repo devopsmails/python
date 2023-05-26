1d2641b0e4d7af57d641bbf7b1944b78b4b983d9
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-05-26 14:54:52 +0530

    Create selenium_setup.py

---
+++ python_methods/selenium_webdriver/selenium_setup.py
@@ -0,0 +1,15 @@
+First need to install Google Chrome
+
+second need to install Chrome Driver:
+  https://chromedriver.chromium.org/downloads
+    ==> Need to select the same version of the Google Chrome Browser.
+    ==> Need to select windows version.
+    ==> After downlaoding need to extract the file.
+    main.py:
+    =======
+      Need to setup 
+      chrome_driver_path = chrome_driver_path = "S:\Devops\Downloading files(temp)\chrome_driver_folder\chromedriver.exe"
+ ===========
+selenium installation:
+===========
+import selenium
