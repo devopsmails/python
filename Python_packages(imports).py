350a5e9061d08a392f3181f7405e86cc4a9c0aaf
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-05-30 20:41:05 +0530

    Update Python_packages(imports).py

--- Python_packages(imports).py
+++ Python_packages(imports).py
@@ -8,7 +8,12 @@ import smtplib #helps sending mails
 import datetime #Helps to cath hold the date& time to execute specific action
 import requests # helps to call the api requests
 from bs4 import BeautifulSoup #for web scraping
+
 impor selenium #web testing
+from selenium import webdriver #for chrome webscrapng
+from selenium.webdriver.common.by import By #importing by key for locating element
+
+
 
 pyperclip :(Helps to copy the info to clipboard).
 https://pypi.org/project/pyperclip/
