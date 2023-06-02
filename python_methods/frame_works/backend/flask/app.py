aa5c27a1f6ca1aee4929d198c7d5edf1b3829709
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-06-02 15:37:55 +0530

    Update and rename python_methods/frame_works/backend/main.py to python_methods/frame_works/backend/flask/app.py

---
+++ python_methods/frame_works/backend/flask/app.py
@@ -0,0 +1,23 @@
+Flask: 
+  Flask is a micro web framework written in Python. 
+  It is classified as a microframework because it does not require particular tools or libraries.
+  We can write a function execute on web server.
+  ==> It convert normal function to HTML file.
+  ex:
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+  
+  click on the ip address: 
+result on webbrowser:
+  browser: top right three vertical dots > more tools > Developer tools > Elements
+we can see the function coverted to HTML
+
+<html>
+  <body>Hello,world</body>
+</html>
