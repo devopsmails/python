2c3e2038a457663e7c7e8597ef37e9434e2a5981
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-06-14 16:54:05 +0530

    Update Advanced_decorator_fucntions_with_args_&_kwargs.py

--- python_methods/frame_works/backend/flask/Advanced_decorator_fucntions_with_args_&_kwargs.py
+++ python_methods/frame_works/backend/flask/Advanced_decorator_fucntions_with_args_&_kwargs.py
@@ -18,3 +18,19 @@ def create_blog_post(user):
 new_user = User("angela")
 new_user.is_logged_in = True
 create_blog_post(new_user)
+
+
+ex2:
+def logging_decorator(fn):
+    def wrapper(*args, **kwargs):
+        print(f"You called {fn.__name__}{args}")
+        result = fn(args[0], args[1], args[2])
+        print(f"It returned: {result}")
+    return wrapper
+
+
+@logging_decorator
+def a_function(a, b, c):
+    return a * b * c
+    
+a_function(1, 2, 3)
