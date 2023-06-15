3692ac7cf183ff554b701b114d8970c259a7bedc
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-06-15 12:37:43 +0530

    Create edit_website_template_online.py

---
+++ python_methods/frame_works/backend/flask/edit_website_template_online.py
@@ -0,0 +1,4 @@
+If we want to edit any website template oneline on any web browser:
+  right click > inspect > console >cmd:
+    document.body.contentEditable=true + Enter
+Then we can edit anything online & want see the same changes from next time need to: right click > save the file > replace the index.html with changed filename.html
