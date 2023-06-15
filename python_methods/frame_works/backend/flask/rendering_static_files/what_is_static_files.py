9697c1373c9fe8bfcbec402dcadaa8dc00d54a77
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-06-15 11:37:40 +0530

    Create what_is_static_files.py

---
+++ python_methods/frame_works/backend/flask/rendering_static_files/what_is_static_files.py
@@ -0,0 +1,5 @@
+if any images or html templates need to render, 
+Flask will look for static folder for images
+                    templates folder for html templates. 
+
+Ex: examples are at the next files.
