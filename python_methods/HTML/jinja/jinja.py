aa1facbbf110ce770bc1661436133b84e896f521
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-06-16 16:55:55 +0530

    Create jinja.py

---
+++ python_methods/HTML/jinja/jinja.py
@@ -0,0 +1,8 @@
+what is Jinja?
+  => helps to produce Dynamic HTML pages.
+  => Jinja is a fast, expressive, extensible templating engine. 
+  => helps to execute python code by rendering in "{{ #what ever the code mentioned will be executed ex: 5*6 ans: 30 }}"
+     Special placeholders in the template allow writing code similar to Python syntax. 
+     Then the template is passed data to render the final document.
+Link:
+  https://jinja.palletsprojects.com/en/3.1.x/
